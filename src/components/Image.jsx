b8a1ee57b3edd375032e00bdb846ca5ba13a2852
Udeorah Frank <franklynudeorah@yahoo.com>
2022-10-29 22:14:37 +0100

    added id prop

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Image =({src, alt, className}) => {
+const Image =({src, alt, className, id}) => {
   return (
     <>
-      <img src={src} alt={alt} className={className}/>
+      <img src={src} alt={alt} className={className} id={id}/>
     </>
   )
 }
